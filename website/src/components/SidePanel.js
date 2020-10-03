bc764c081f1194e5ea50d05a1df02e597ead3c50
ddunn2 <dd4dev@gmail.com>
2020-10-03 10:05:54 +0100

    Add collapsible sidepanel sections

--- website/src/components/SidePanel.js
+++ website/src/components/SidePanel.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import data from "../../content/learn/docs-list.yaml";
 
 export default function SidePanel() {
-  const content = buildSidePanel();
-  return <aside>{content}</aside>;
+  return (
+    <aside>
+      {data.map((doc, index) => {
+        return (
+          <SidePanelSection
+            key={`${doc.title}-${index}`}
+            title={doc.title}
+            docItems={doc.items}
+          />
+        );
+      })}
+    </aside>
+  );
 }
 
-function buildSidePanel() {
-  const sidePanelContent = data.map((doc) => {
-    return (
-      <>
-        <h2>{doc.title}</h2>
-        <ul>
-          {doc.items.map((docItem) => {
-            return (
-              <li>
-                <Link to={docItem.link}>{docItem.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </>
-    );
-  });
+function SidePanelSection({ title, docItems }) {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-  return sidePanelContent;
+  return (
+    <section>
+      <h2 onClick={() => setIsExpanded(!isExpanded)}>{title}</h2>
+      <ul
+        style={{ overflow: "hidden", height: isExpanded ? "min-content" : 0 }}
+      >
+        {docItems.map((docItem, index) => {
+          return (
+            <li key={`${docItem.title}-${index}`}>
+              <Link to={docItem.link}>{docItem.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
