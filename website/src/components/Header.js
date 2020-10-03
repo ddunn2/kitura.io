8aeaec20a58e4c25b8fee2df43f810196c329dbc
ddunn2 <dd4dev@gmail.com>
2020-10-03 14:39:42 +0100

    Add sass to project + styling for header component

--- website/src/components/Header.js
+++ website/src/components/Header.js
@@ -1,27 +1,66 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import Logo from "../images/kitura.svg";
+import Twitter from "../images/twitter_icon.svg";
+import GitHub from "../images/github_icon.svg";
+import Slack from "../images/slack_icon.svg";
+
 const Header = () => {
   return (
-    <header>
-      <h1>
-        <Link to="/">Kitura</Link>
-      </h1>
-      <nav>
-        <ul>
+    <header className="header">
+      <nav className="header--nav">
+        <ul className="header--nav_list">
           <li>
-            <Link to="/learn">Learn</Link>
+            <Link className="header--logo header--nav_link" to="/">
+              <img className="header--logo_img" src={Logo}></img>
+              <h1 className="header--logo_title">KITURA</h1>
+            </Link>
           </li>
           <li>
-            <Link to="/news">News</Link>
+            <Link className="header--nav_link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="header--nav_link" to="/learn">
+              Learn
+            </Link>
+          </li>
+          <li>
+            <Link className="header--nav_link" to="/news">
+              News
+            </Link>
           </li>
         </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li>GitHub</li>
-          <li>Slack</li>
-          <li>Twitter</li>
+        <ul className="header--nav_list">
+          <li>
+            <a
+              className="header--nav_social_link"
+              target="_blank"
+              href="https://github.com/Kitura"
+            >
+              <img src={GitHub}></img>
+            </a>
+          </li>
+          <li>
+            <a
+              className="header--nav_social_link"
+              target="_blank"
+              href="https://twitter.com/KituraDev"
+            >
+              <img src={Twitter}></img>
+            </a>
+          </li>
+          <li>
+            <a
+              className="header--nav_social_link"
+              target="_blank"
+              href="https://swift-at-ibm.slack.com"
+            >
+              <img src={Slack}></img>
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
--- website/src/components/Layout.js
+++ website/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import "../styles/main.scss";
 
 const Layout = ({ children }) => {
   return (
